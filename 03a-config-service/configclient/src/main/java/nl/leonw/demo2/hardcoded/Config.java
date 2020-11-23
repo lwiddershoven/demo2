0023c1459012bead3b0c16ee17ead861781390d0
Leon <qaz@dds.nl>
2020-11-23 19:42:06 +0100

    Working config client

---
+++ 03a-config-service/configclient/src/main/java/nl/leonw/demo2/hardcoded/Config.java
@@ -0,0 +1,15 @@
+package nl.leonw.demo2.hardcoded;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    // This is empty
+    // The beans we made in the previous project?
+
+    // Just having the maven dependency om spring data JDBC tells Spring we want a datasource and template
+
+    // See JdbcTemplateAutoConfiguration
+    // See spring-autoconfigure-metadata.properties
+}
